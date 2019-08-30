25cf5a0835e51fd37a0295ea49f83cb251aab6cc
lc <1259493451@qq.com>
2019-08-30 19:46:00 +0800

    轮播图组件完成

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 // 解决移动端click事件300ms 延迟问题
 import fastClick from 'fastclick'
+// 引入vue-awesome-swiper 插件 来提供轮播图的功能
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 // import store from './store'
 // 引入reset.css 来初始化  基础样式
@@ -11,11 +12,13 @@ import 'styles/reset.css'
 import 'styles/border.css'
 // 字体样式文件的引入
 import 'styles/iconfont.css'
+// 要使用vue-awesome-swiper 插件 还要引入swiper.css
 import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 // 解决移动端click事件300ms延迟问题
 fastClick.attach(document.body)
+// 需要通过Vue.use 来挂载VueAwesomeSwiper这个插件
 Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide v-for="item in swiperList" :key="item.id">
+                <img  class="swiper-img" :src="item.imgUrl" alt="">
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeSwiper",
+        data () {
+            return {
+                swiperOption: {
+                    pagination: '.swiper-pagination',
+                    loop: true
+                },
+                swiperList: [
+                    {
+                        id: '001',
+                        imgUrl: 'https://source.qunarzz.com/site/images/wns/20190827_dujia_homepage_750x192_2.jpg'
+                    },
+                    {
+                        id: '002',
+                        imgUrl: 'https://source.qunarzz.com/site/images/wns/20190826_qunar_dujia_750x192_3.jpg'
+                    },
+                    {
+                        id: '003',
+                        imgUrl: 'https://source.qunarzz.com/site/images/wns/20190801_qunar_dujia_750x192_6_V1.jpg'
+                    },
+                    {
+                        id: '004',
+                        imgUrl: 'https://source.qunarzz.com/site/images/wns/20190809_dujia_homepage_top_banner_5.jpg'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff !important
+    .wrapper
+        overflow:hidden
+        width:100%
+        height: 0
+        padding-bottom:25%
+        background:#eee
+        .swiper-img
+            width: 100%
+
+</style>
