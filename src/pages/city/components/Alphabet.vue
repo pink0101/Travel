c062e806aaca80c8518db17059abb74b50b281eb
lc <1259493451@qq.com>
2019-09-04 12:42:13 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul class="list">
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "CityAlphabet"
+    }
+</script>
+
+<style lang="stylus" scoped>
+    @import "~styles/varibles.styl";
+    .list
+        display:flex
+        flex-direction:column /* 设置主轴为垂直方向 */
+        justify-content:center/* 在主轴上居中 */
+        position:absolute
+        top:0
+        right:0
+        bottom:0
+        width:.4rem
+        .item
+            line-height:.44rem
+            text-align:center
+            color:$bgColor
+</style>
