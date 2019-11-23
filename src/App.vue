d23e51c36b664db09f143e7860fed7ca165a6590
lc <1259493451@qq.com>
2019-11-23 09:14:02 +0800

    finish pages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <keep-alive>
+    <keep-alive> <!-- vue自带的标签 路由的内容加载过后，就放到内存中，避免生命钩子多次去获取数据，影响网页的性能 -->
       <!-- router-view 显示的是当前路由对应的内容 -->
       <router-view/>
     </keep-alive>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@
     import HomeRecommend from './components/Recommend'
     import HomeWeekend from './components/Weekend'
     import axios from 'axios'
+    import { mapState } from 'vuex'
 
     export default {
         name: 'Home',
@@ -27,16 +28,20 @@
         },
         data() {
             return {
+                lastCity: '',
                 swiper: [],
                 icons: [],
                 recommend: [],
                 weekend: []
             }
         },
+        computed: {
+            ...mapState(['city'])
+        },
         methods: { // vue中的方法定义在 methods中
             getHomeInfo() {
                 // axios 返回结果是一个promise对象 所以这里使用 .then 接收
-                axios.get('/api/index.json') // 这里的接口采用了 接口转发机制  配置见 config/index.js proxyTable 对象
+                axios.get('/api/index.json?city=' + this.city) // 这里的接口采用了 接口转发机制  配置见 config/index.js proxyTable 对象
                     .then(this.getHomeInfoSucc)
             },
             getHomeInfoSucc(res) {
@@ -51,7 +56,14 @@
             }
         },
         mounted() { // 生命周期函数 vue页面挂载之后 自动执行该函数
+            this.lastCity = this.city // 将获取到的数据城市数据进行一个保存
             this.getHomeInfo()
+        },
+        activated () { // 当vue使用了keep-alive 后 加载过的路由会放到内存中，当我们重新选择城市时，需要重新获取数据 此时使用 activated 生命周期钩子
+            if (this.lastCity !== this.city) {
+                this.lastCity = this.city
+                this.getHomeInfo() // 发送 axios 请求
+            }
         }
     }
 </script>
